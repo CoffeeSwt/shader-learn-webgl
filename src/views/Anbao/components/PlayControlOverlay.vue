bc228888688f13cb886a036653aa4a3d00188392
Coffeesw <897884964@qq.com>
2026-03-06 10:09:47 +0800

    ui fix

--- src/views/Anbao/components/PlayControlOverlay.vue
+++ src/views/Anbao/components/PlayControlOverlay.vue
@@ -30,7 +30,7 @@ const formatTime = (t: number) => {
 <style scoped>
 #play-control-overlay {
     position: absolute;
-    bottom: 30px;
+    bottom: 120px;
     left: 50%;
     transform: translateX(-50%);
     gap: 15px;
